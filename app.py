1af52304b5f4c690ef55b5c880b5fae9212d2ae0
Jomari Macaraig <jomari@auxahelth.com>
2024-06-24 23:17:10 +0800

    topic9: If else

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-price = 30
+age = 31
 
-print(price > 10 and price < 30)
-print(price > 10 or price < 30)
-print(not price < 10)
+if age > 30:
+    print("You're in your thirties")
+elif age > 20:
+    print("You're in your twenties")
+    print("You're a young adult")
+elif age > 13:
+    print("You're a teenager")
+    print("Best years of your life")
+else:
+    print("You're a child")
+
+print("Done")
