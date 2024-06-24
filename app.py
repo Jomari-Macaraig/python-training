314468e36054f73b33b100309c576b29a82ca516
Jomari Macaraig <jomari@auxahelth.com>
2024-06-24 22:35:21 +0800

    topic6: Update script

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 print(20 + 15)
-print(20 - 15)
-print(20 * 15)
-print(20 / 15)
-print(20 // 15)
-print(20 % 15)
+# print(20 - 15)
+# print(20 * 15)
+# print(20 / 15)
+# print(20 // 15)
+# print(20 % 15)
+# print(2 ** 3)
+#
+# value = 10
+# value = value + 2
+# value += 2
+# print(value)
