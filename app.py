58233ebf414ed79a26135d0e459d5f5babfe99be
Jomari Macaraig <jomari@auxahelth.com>
2024-06-25 00:01:08 +0800

    topic10: while loops

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-age = 31
-
-if age > 30:
-    print("You're in your thirties")
-elif age > 20:
-    print("You're in your twenties")
-    print("You're a young adult")
-elif age > 13:
-    print("You're a teenager")
-    print("Best years of your life")
-else:
-    print("You're a child")
-
-print("Done")
+i = 1
+while i <= 10:
+    # print(i * "*")
+    print(i)
+    i += 1
