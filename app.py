f830ecce8d0902383fd336a9586b04d517462188
Jomari Macaraig <jomari@auxahelth.com>
2024-06-25 17:18:35 +0800

    topic13: tuples

--- app.py
+++ app.py
@@ -1,30 +1,7 @@
-numbers = [1, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9, 9]
+numbers = (1, 2, 3)
 
-unique_numbers = set(numbers)
+# numbers = (1, )
 
-# mathematics = [
-#     "Charlotte",
-#     "Lewis",
-#     "Olivia",
-#     "Eddie",
-#     "Sophia",
-#     "Liam",
-#     "Brea",
-#     "Avery",
-#     "Noah",
-# ]
-#
-# economics = [
-#     "Isabella",
-#     "Mia",
-#     "Eric",
-#     "Sophia",
-#     "Liam",
-#     "Brea",
-#     "Avery",
-#     "Noah",
-# ]
-#
-# print(set(mathematics).difference(set(economics)))
-# print(set(mathematics).intersection((set(economics))))
-# print(set(economics).difference((set(mathematics))))
+# numbers[0] = 5
+
+print(numbers)
