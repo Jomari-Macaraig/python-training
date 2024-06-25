f886e4a5858923581d1db24f907ca31186bcc398
Jomari Macaraig <jomari@auxahelth.com>
2024-06-25 17:20:37 +0800

    topic15: Dictionary

--- app.py
+++ app.py
@@ -1,30 +1,14 @@
-numbers = [1, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9, 9]
+days = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+}
 
-unique_numbers = set(numbers)
 
-# mathematics = [
-#     "Charlotte",
-#     "Lewis",
-#     "Olivia",
-#     "Eddie",
-#     "Sophia",
-#     "Liam",
-#     "Brea",
-#     "Avery",
-#     "Noah",
-# ]
-#
-# economics = [
-#     "Isabella",
-#     "Mia",
-#     "Eric",
-#     "Sophia",
-#     "Liam",
-#     "Brea",
-#     "Avery",
-#     "Noah",
-# ]
-#
-# print(set(mathematics).difference(set(economics)))
-# print(set(mathematics).intersection((set(economics))))
-# print(set(economics).difference((set(mathematics))))
+# days[5] = "Friday"
+# print(days.keys())
+# print(days.values())
+# days[1] = "monday"
+
+print(days)
